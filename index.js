bc3f3a584336fd31785653095468849c3e3ff227
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-10-19 16:37:48 +0300

    Fetch data from data

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
 
-var mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '606969',
-    datasbase: 'Employeedb'
+app.use(bodyparser.json());
+
+const mysqlConnection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "606969",
+    datasbase: "Employeedb"
 });
 
 mysqlConnection.connect((err) => {
@@ -12,4 +17,15 @@ mysqlConnection.connect((err) => {
         console.log('DB connection successful');
     else
         console.log('DB connection failed \n Error : ' + JSON.stringify(err, undefined, 2));
+});
+
+app.listen(3000, () => console.log('Express server is running at port no : 3000'));
+
+app.get('/employees', (req, res) => {
+    mysqlConnection.query('SELECT * FROM Employeedb.employee', (err, rows, fields) => {
+        if (!err)
+            console.log(rows);
+        else
+            console.log(err);
+    });
 });
