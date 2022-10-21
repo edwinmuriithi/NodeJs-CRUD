bbc2f1e3d32599ee1fe625b7cfc4efaceb947d2c
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-10-21 12:28:36 +0300

    Add Update and Post functionality

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const mysqlConnection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "606969",
-    datasbase: "Employeedb"
+    datasbase: "Employeedb",
+    multipleStatements: true
 });
 
 mysqlConnection.connect((err) => {
@@ -21,10 +22,55 @@ mysqlConnection.connect((err) => {
 
 app.listen(3000, () => console.log('Express server is running at port no : 3000'));
 
+//Get all Employees
 app.get('/employees', (req, res) => {
     mysqlConnection.query('SELECT * FROM Employeedb.employee', (err, rows, fields) => {
         if (!err)
-            console.log(rows);
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+//Get an Employee usng id
+app.get('/employees/:id', (req, res) => {
+    mysqlConnection.query('SELECT * FROM Employeedb.employee WHERE EmpID =?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    });
+});
+
+//Delete an employee using id
+app.delete('/employees/:id', (req, res) => {
+    mysqlConnection.query('DELETE FROM Employeedb.employee WHERE EmpID = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send('Deleted Successfully');
+        else
+            console.log(err);
+    });
+});
+
+//Insert an employee 
+app.post('/employees', (req, res) => {
+    let data = { Name: req.body.Name, EmpCode: req.body.EmpCode, Salary: req.body.Salary };
+    let sqlQuery = "INSERT INTO Employeedb.employee SET ?";
+    mysqlConnection.query(sqlQuery, data, (err, results) => {
+        if (!err)
+            res.send(results);
+        else
+            console.log(err);
+    });
+});
+
+//Update an employee 
+app.put('/employees/:id', (req, res) => {
+    let sqlQuery = "UPDATE Employeedb.employee SET Name='" + req.body.Name + "',EmpCode='" + req.body.EmpCode + "',Salary='" + req.body.Salary + "'WHERE EmpID=" + req.params.id;
+    mysqlConnection.query(sqlQuery, (err, results) => {
+
+        if (!err)
+            res.send(results);
         else
             console.log(err);
     });
